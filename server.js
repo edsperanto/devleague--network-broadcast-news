9cd44b596b7d3f285246da1d34d67882c742d551
Edward Gao <egao@sent.com>
2016-12-26 22:42:00 -1000

    authentication step system set up

--- server.js
+++ server.js
@@ -6,22 +6,41 @@ let server = net.createServer(function(socket) {
 
 	let address = socket.address().address;
 	let port = socket.address().port;
+	let step = 1;
 
 	socketList.push(socket);
 	socket.setEncoding('utf8');
 
 	console.log(`CONNECTED: ${address}:${port}`);
 	socket.write(`CONNECTED TO: ${address}:${port}`);
-	
+
+	socket.write('Welcome to the shadow web! Do you have an account? [Y/n]');
 	socket.on('data', (chunk) => {
-		chunk = chunk.substr(0, chunk.length - 1);
-		broadcast(chunk, socket, address, port);
-		console.log(`SERVER BCAST FROM ${address}:${port} : ${chunk}`);
+		chunk = cleanse(chunk);
+		switch(step) {
+			case 1:
+				if(chunk !== 'n') {
+					socket.write('LOGIN SUCCESSFUL!');
+					step = 99;
+				}
+				break;
+			case 99:
+				broadcast(chunk, socket, address, port);
+				console.log(`SERVER BCAST FROM ${address}:${port} : ${chunk}`);
+				break;
+			default:
+				console.log('ERROR: STEP DOES NOT EXIST');
+				break;
+		}
 	});
 
 	process.stdin.on('data', (cmd) => {
 		socket.write(cmd);
 	});
+
+	// socket.write("LOGIN UNSUCCESSFUL, YOU WILL NOW BE DISCONNECTED FROM THE SHADOW WEB");
+	// socketListPop(socket);
+	// socket.destroy();
 
 	socket.on('end', () => {
 		console.log(`CLOSED: ${address}:${port}`);
@@ -30,9 +49,9 @@ let server = net.createServer(function(socket) {
 
 });
 
-function broadcast(msg, origin, address, port) {
+function broadcast(msg, user, address, port) {
 	for(let i = 0; i < socketList.length; i++) {
-		if(socketList[i] === origin) {
+		if(socketList[i] === user) {
 			socketList[i].write("MESSAGE SENT");
 		}else{
 			socketList[i].write(`${address}:${port}: "${msg}"`);
@@ -40,9 +59,14 @@ function broadcast(msg, origin, address, port) {
 	}
 }
 
-function socketListPop(origin) {
-	let i = socketList.indexOf(origin);
+function socketListPop(user) {
+	let i = socketList.indexOf(user);
 	socketList.splice(i, 1);
+}
+
+function cleanse(chunk) {
+	chunk = chunk.substr(0, chunk.length - 1);
+	return chunk.trim();
 }
 
 server.listen(6969, '0.0.0.0', function() {
