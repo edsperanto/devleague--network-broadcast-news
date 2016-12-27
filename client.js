6831083d898280a23bf80f4da8f1374ab1d586c5
Edward Gao <egao@sent.com>
2016-12-26 21:53:01 -1000

    improved format

--- client.js
+++ client.js
@@ -4,7 +4,6 @@ let client = net.createConnection(6969, 'localhost');
 client.setEncoding('utf8');
 
 client.on('connect', () => {
-	console.log('connected');
 	process.stdin.pipe(client);
 });
 
--- server.js
+++ server.js
@@ -4,11 +4,19 @@ let socketList = [];
 
 let server = net.createServer(function(socket) {
 
+	let address = socket.address().address;
+	let port = socket.address().port;
+
 	socketList.push(socket);
 	socket.setEncoding('utf8');
+
+	console.log(`CONNECTED: ${address}:${port}`);
+	socket.write(`CONNECTED TO: ${address}:${port}`);
 	
 	socket.on('data', (chunk) => {
-		broadcast(chunk);
+		chunk = chunk.substr(0, chunk.length - 1);
+		broadcast(chunk, socket, address, port);
+		console.log(`SERVER BCAST FROM ${address}:${port} : ${chunk}`);
 	});
 
 	process.stdin.on('data', (cmd) => {
@@ -16,18 +24,27 @@ let server = net.createServer(function(socket) {
 	});
 
 	socket.on('end', () => {
-		console.log(`server disconnected from client`);
+		console.log(`CLOSED: ${address}:${port}`);
+		socketListPop(socket);
 	});
 
 });
 
-function broadcast(msg) {
+function broadcast(msg, origin, address, port) {
 	for(let i = 0; i < socketList.length; i++) {
-		socketList[i].write(msg);
-		console.log(msg);
+		if(socketList[i] === origin) {
+			socketList[i].write("MESSAGE SENT");
+		}else{
+			socketList[i].write(`${address}:${port}: "${msg}"`);
+		}
 	}
 }
 
+function socketListPop(origin) {
+	let i = socketList.indexOf(origin);
+	socketList.splice(i, 1);
+}
+
 server.listen(6969, '0.0.0.0', function() {
-	console.log('opened server on', this.address());
+	console.log(`Server listening on ${this.address().address}:${this.address().port}`);
 });
